1e9c8eb26c54e7a527c8cb47464d7aa320cda940
Jules <jules.belletre@gmail.com>
2022-12-17 16:21:21 +0100

    add the switch case for user inputs

--- skript.sh
+++ skript.sh
@@ -11,23 +11,31 @@ else
 	fi
 fi
 ./c.o #launch the .c file 
+t1=0 ; t2=0 ; t3=0 ; p1=0 ; p2=0 ; p3=0 ; w=0 ; m=0 ; h=0 ; F=0 ; G=0 ; S=0 ; A=0 ; O=0 ; Q=0 ; tab=0 ; abr=0 ; avl=0 ;
 var=0
 for var in $(seq 1 "$#") ; do
 	case "${!var}" in
 		# TYPE DE DONNE A ENVOYER AU C
-		'-t') echo "on veut la température" ;;
-		'-p') echo "on veut la pression" ;;
-		'-w') echo "on veut le vent" ;;
-		'-m') echo "on veut l'humidité";;
-		'-h') echo "on veut l'altitude";;
+		'-t1') echo "on veut la température" ; t1=1 ;;
+		'-t2') echo "on veut la température" ; t2=1 ;;
+		'-t3') echo "on veut la température" ; t3=1 ;;
+		'-p1') echo "on veut la pression" ; p1=1 ;;
+		'-p2') echo "on veut la pression" ; p2=1 ;;
+		'-p3') echo "on veut la pression" ; p3=1 ;;
+		'-w') echo "on veut le vent" ; w=1 ;;
+		'-m') echo "on veut l'humidité" ; m=1 ;;
+		'-h') echo "on veut l'altitude" ; h=1 ;;
 		# GEOGRAPHIE DES DONNES A FILTRER AVANT D'ENVOYER AU C
-		'-F') echo "France métropolitaine";;
-		'-G') echo "Guyane" ;; 
-		'-S') echo "Saint-Pierre et Michelin" ;;
-		'-A') echo "Antilles" ;;
-		'-O') echo "Ocean indien" ;;
-		'-Q') echo "Antartique" ;;
-	  *   ) echo "aller vasy ça marche pas" ; exit 0 ;;
+		'-F') echo "France métropolitaine" ; F=1 ;;
+		'-G') echo "Guyane" ; G=1 ;; 
+		'-S') echo "Saint-Pierre et Michelin" ; S=1;;
+		'-A') echo "Antilles" ; A=1 ;;
+		'-O') echo "Ocean indien" ; O=1 ;;
+		'-Q') echo "Antartique" ; Q=1 ;;
+		# TYPE DE TRI DEMANDE 
+		'--tab') echo "Le type de tri demandé est par un tableau ou une liste chainée" ; tab=1 ;;
+		'--abr') echo "Le type de tri demandé est un abr" ; abr=1 ;;
+		'--avl') echo "Le type de tri demandé est un avl" ; avl=1 ;;
+		 *  ) echo "l'argument ${!var} n'est pas réferencé" ; exit 0 ;;
 	esac
 done
-
