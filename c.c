3767daa5d31748de332cc52caf5bd55c0a11940c
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 19:13:01 +0100

    worked on the c file and add a part in the sh one

--- c.c
+++ c.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
-	puts("hello world");
+int main(int argc, char **argv) {
+	if(argc!=2){
+		puts("Erreur de argc");
+		exit(1);
+	}
+	FILE *fichier=NULL;
+	fichier = fopen("data.csv", "r+");
+	fclose(fichier); 
 	return 0 ;
 }
-
