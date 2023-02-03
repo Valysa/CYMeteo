b7dc7447ba0793888652359cc11b8b8d1ac52447
Jules <jules.belletre@gmail.com>
2023-02-03 05:22:03 +0100

    worked on test for user imputs

--- skript.sh
+++ skript.sh
@@ -27,106 +27,126 @@ testArea (){
 	return 0
 }
 
-t1=0 ; t2=0 ; t3=0 ; p1=0 ; p2=0 ; p3=0 ; w=0 ; m=0 ; h=0 ; F=0 ; G=0 ; S=0 ; A=0 ; O=0 ; Q=0 ; tab=0 ; abr=0 ; avl=0 ; d=0; t=0; file=0; f=0;
+t1=0 ; t2=0 ; t3=0 ; p1=0 ; p2=0 ; p3=0 ; w=0 ; m=0 ; h=0 ; F=0 ; G=0 ; S=0 ; A=0 ; O=0 ; Q=0 ; tab=0 ; abr=0 ; avl=0 ; d=0; t=0; file=0; f=0; skip=0;
 var=0 ; nbExecC=0 ; nbLoca=0; nbarg=0; sday=0 ; smonth=0 ; syear=0 ; eday=0; emonth=0; eyear=0; sdate=0; edate=0; a=0; g=0; slong=0 ; slat=0; elong=0 ; elat=0 ;
 nameOutpout=data.txt
 for var in $(seq 1 "$#") ; do
 	nbarg=$var
-	case "${!var}" in
-		#FICHIER D'ENTREE
-		'-f') if [ "$f" -eq "0" ] ; then 
-				nbarg=$((nbarg+1)) ; f=1 ;
-				if [ -e ${!nbarg} ] ; then
-					file=${!nbarg}
+	if [ "$skip" -gt 0 ] ; then
+		skip=$((skip-1))
+	else  
+		case "${!var}" in
+			#FICHIER D'ENTREE
+			'-f') if [ "$f" -eq "0" ] ; then 
+					skip=$((skip+1)) ;
+					nbarg=$((nbarg+1)) ; f=1 ;
+					if [ -e ${!nbarg} ] ; then
+						file=${!nbarg}
+					else
+						echo "the file you put in entry does not exist or isnt valid"
+						exit;
+					fi
 				else
-					echo "the file you put in entry does not exist or isnt valid"
-					exit;
-				fi
-			else
-				if [ -e meteo_filtered_data_v1.csv ] ; then
-					echo "you didn't select a file but meteo_filtered_data_v1.csv looks valid, we are using it"
-					file=meteo_filtered_data_v1.csv
+					if [ -e meteo_filtered_data_v1.csv ] ; then
+						echo "you didn't select a file but meteo_filtered_data_v1.csv looks valid, we are using it"
+						file=meteo_filtered_data_v1.csv
+					else
+						echo "you need to specify a valid file with -f, --help for help"
+						exit 1
+					fi
+				fi ;;
+			# TYPE DE DONNE A ENVOYER AU C
+			'-t1') if test $t1 ; then 
+						echo "on veut la température" ; t1=1 ; nbExecC=$((nbExecC+1))
+				fi ;;
+			'-t2') if test $t2 ; then 
+						echo "on veut la température" ; t2=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-t3') if test $t3 ; then 
+						echo "on veut la température" ; t3=1 ; nbExecC=$((nbExecC+1))
+					fi ;;
+			'-p1') if test $p1 ; then 
+						echo "on veut la pression" ; p1=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-p2') if test $p2 ; then 
+						echo "on veut la pression" ; p2=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-p3') if test $p3 ; then 
+						echo "on veut la pression" ; p3=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-w') if test $w ; then 
+						echo "on veut le vent" ; w=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-m') if test $m ; then 
+						echo "on veut l'humidité" ; m=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			'-h') if test $h ; then 
+						echo "on veut l'altitude" ; h=1 ; nbExecC=$((nbExecC+1)) 
+					fi ;;
+			# RECUPERATION DES DONNEES GEOGRAPHIQUES
+			'-F') if testArea $nbLoca ; then 
+					echo "France métropolitaine" ; F=1 ;  nameOutpout=France.txt 
+				fi ;;
+			'-G') if testArea $nbLoca ; then 
+					echo "Guyane" ; G=1 ;  nameOutpout=Guyane.txt
+				fi ;;
+			'-S') if testArea $nbLoca ; then 
+					echo "Saint-Pierre et Michelin" ; S=1 ;  nbLoca=$((nbLoca+1))  ;   nameOutpout=Saint_Pierre_Et_Miquelon.txt
+				fi ;;
+			'-A') if testArea $nbLoca ; then 
+					echo "Antilles" ; A=1 ; nbLoca=$((nbLoca+1)) ;  nameOutpout=Antilles.txt
+				fi ;;
+			'-O') if testArea $nbLoca ; then 
+					echo "Ocean indien" ; O=1 ; nbLoca=$((nbLoca+1)) ;  nameOutpout=Ocean_Indien.txt
+				fi ;;
+			'-Q') if testArea $nbLoca ; then 
+					echo "Antarctique" ; Q=1 ;  nameOutpout=Antarctique.txt
+				fi ;;
+			'-g') if [ "$g" -eq "0" ] ; then 
+					skip=$((skip+1))
+					nbarg=$((nbarg+1)) ; g=1 ;
+					slong=${!nbarg} ; nbarg=$((nbarg+1)) ; skip=$((skip+1)) ; elong=${!nbarg} ;
 				else
-					echo "you need to specify a valid file with -f, --help for help"
+					echo can t put that many restriction for more info type --help
 					exit 1
-				fi
-			fi ;;
-		# TYPE DE DONNE A ENVOYER AU C
-		'-t1') if test $t1 ; then 
-					echo "on veut la température" ; t1=1 ; nbExecC=$((nbExecC+1))
-			   fi ;;
-		'-t2') if test $t2 ; then 
-					echo "on veut la température" ; t2=1 ; nbExecC=$((nbExecC+1)) 
 				fi ;;
-		'-t3') if test $t3 ; then 
-					echo "on veut la température" ; t3=1 ; nbExecC=$((nbExecC+1))
+			'-a') if [ "$a" -eq "0" ] ; then 
+				skip=$((skip+1)) ;
+					nbarg=$((nbarg+1)) ; a=1 ;
+					slat=${!nbarg} ; nbarg=$((nbarg+1)) ; skip=$((skip+1)) ; elat=${!nbarg} ;
+				else
+					echo "can t put that many restriction for more info type --help"
+					exit 1
 				fi ;;
-		'-p1') if test $p1 ; then 
-					echo "on veut la pression" ; p1=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		'-p2') if test $p2 ; then 
-					echo "on veut la pression" ; p2=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		'-p3') if test $p3 ; then 
-					echo "on veut la pression" ; p3=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		'-w') if test $w ; then 
-					echo "on veut le vent" ; w=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		'-m') if test $m ; then 
-					echo "on veut l'humidité" ; m=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		'-h') if test $h ; then 
-					echo "on veut l'altitude" ; h=1 ; nbExecC=$((nbExecC+1)) 
-				fi ;;
-		# RECUPERATION DES DONNEES GEOGRAPHIQUES
-		'-F') if testArea $nbLoca ; then 
-				echo "France métropolitaine" ; F=1 ;  nameOutpout=France.txt 
-			  fi ;;
-		'-G') if testArea $nbLoca ; then 
-				echo "Guyane" ; G=1 ;  nameOutpout=Guyane.txt
-			  fi ;;
-		'-S') if testArea $nbLoca ; then 
-				echo "Saint-Pierre et Michelin" ; S=1 ;  nbLoca=$((nbLoca+1))  ;   nameOutpout=Saint_Pierre_Et_Miquelon.txt
-			  fi ;;
-		'-A') if testArea $nbLoca ; then 
-				echo "Antilles" ; A=1 ; nbLoca=$((nbLoca+1)) ;  nameOutpout=Antilles.txt
-			  fi ;;
-		'-O') if testArea $nbLoca ; then 
-				echo "Ocean indien" ; O=1 ; nbLoca=$((nbLoca+1)) ;  nameOutpout=Ocean_Indien.txt
-			  fi ;;
-		'-Q') if testArea $nbLoca ; then 
-				echo "Antarctique" ; Q=1 ;  nameOutpout=Antarctique.txt
-			  fi ;;
-		'-g') if [ "$g" -eq "0" ] ; then 
-				nbarg=$((nbarg+1)) ; g=1 ;
-				slong=${!nbarg} ; nbarg=$((nbarg+1)) ; elong=${!nbarg} ;
-			  else
-			  	echo can t put that many restriction for more info type --help
-				exit 1
-			  fi ;;
-		'-a') if [ "$a" -eq "0" ] ; then 
-				nbarg=$((nbarg+1)) ; a=1 ;
-				slat=${!nbarg} ; nbarg=$((nbarg+1)) ; elat=${!nbarg} ;
-			  else
-			  	echo can t put that many restriction for more info type --help
-				exit 1
-			  fi ;;
-		# RECUPERATION DE LA RESTRICTION TEMPORELLE
-		'-d') nbarg=$((nbarg+1)) ; d=1;
-			syear=${!nbarg::4} ; smonth=${!nbarg:5:2} ; sday=${!nbarg:8:2} ; sdate=${!nbarg}T00:00:00+01:00 ;
-			nbarg=$((nbarg+1)) ;
-			eyear=${!nbarg::4} ; emonth=${!nbarg:5:2} ; eday=${!nbarg:8:2} ; edate=${!nbarg}T23:00:00+01:00 ;;
-			[0123456789][0123456789][0123456789][0123456789]-[0123456789][0123456789]-[0123456789][0123456789]) ;;
-		# TYPE DE TRI DEMANDE
-		'--tab') echo "Le type de tri demandé est par un tableau ou une liste chainée" ; tab=1 ;;
-		'--abr') echo "Le type de tri demandé est un abr" ; abr=1 ;;
-		'--avl') echo "Le type de tri demandé est un avl" ; avl=1 ;;
-		# HELP
-		'-help') echo "help à écrire ptdr" ; exit 1 ;;
-		# CAS GENERAL
-	 	#*  ) echo "l'argument ${!var} n'est pas réferencé" ; exit 0 ;;
-	esac
+			# RECUPERATION DE LA RESTRICTION TEMPORELLE
+			'-d') nbarg=$((nbarg+1)) ; d=1;
+				skip=$((skip+1))
+				syear=${!nbarg::4} ; smonth=${!nbarg:5:2} ; sday=${!nbarg:8:2} ; sdate=${!nbarg}T00:00:00+01:00 ;
+				nbarg=$((nbarg+1)) ;
+				skip=$((skip+1))
+				eyear=${!nbarg::4} ; emonth=${!nbarg:5:2} ; eday=${!nbarg:8:2} ; edate=${!nbarg}T23:00:00+01:00 ;;
+			# TYPE DE TRI DEMANDE
+			'--tab') if [ "$tab" -eq 0 ] || [ "$abr" -eq 0 ] || [ "$avl" -eq 0 ] ; then 
+						 tab=1 
+					else	
+						echo "You can't ask for many type of sorting simulteanously, type --help for more info"
+					fi ;;
+			'--abr') if [ "$tab" -eq 0 ] || [ "$abr" -eq 0 ] || [ "$avl" -eq 0 ] ; then 
+						 abr=1 
+					else	
+						echo "You can't ask for many type of sorting simulteanously, type --help for more info"
+					fi ;;
+			'--avl') if [ "$tab" -eq 0 ] || [ "$abr" -eq 0 ] || [ "$avl" -eq 0 ] ; then 
+						 avl=1 
+					else	
+						echo "You can't ask for many type of sorting simulteanously, type --help for more info"
+					fi ;;
+			# HELP
+			'--help') echo "help à écrire ptdr" ; exit 1 ;;
+			# CAS GENERAL
+			*  ) echo "the  ${!var}  argument does not exist, type --help for mor info" ; exit 0 ;;
+		esac
+	fi
 done
 if [ "$f" -eq 0 ] ; then
 	echo " -f must be specified --help for more info"
@@ -206,7 +226,7 @@ if [ "$Q" -eq 1 ] ; then
 	grep -E "89642" area.csv > area_time.csv ; nbLoca=$((nbLoca+1)) ;
 fi
 if [ "$nbExecC" -eq 0 ] ; then
-	echo "vous devez spécifier au moins un type de donnée a traiter, si vous ne savez pas de quoi il s'agit, tapez l'argument -help"
+	echo "You need to specify at least one type of data, type --help for more info"
 	exit 1
 fi
 if [ "$a" -eq "1" ] && [ "$g" -eq "1" ] ; then
@@ -215,16 +235,8 @@ else
 	cat area_time.csv > finale.txt
 fi
 rm area.csv
-var=$(($tab+$abr+$avl))
-echo $var
-echo $nbExecC
-echo $t1
-if [ "$var" -ge 2 ]; then
-	echo "vous ne pouvez pas demander plus d'une restriction géographique"
-	exit 1
-fi
 if [ "$var" -eq 0 ]; then
-	echo "il n'y a pas de manière de trier demandé, le choix par défaut est donc l'avl"
+	echo "no sort method asked, the default one is gonna be avl"
 	avl=1
 fi
 mode=0
